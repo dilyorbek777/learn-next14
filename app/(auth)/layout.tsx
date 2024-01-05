4305035fffd7e016170de17f84c7efedce964729
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2024-01-05 17:37:06 +0500

    Link isActive?�

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+"use client"
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+const navLinks = [
+    { name: "Register", href: "/register" },
+    { name: "Login", href: "/login" },
+];
+
+
+export default function AuthLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const pathname = usePathname();
+    return (
+        <>
+            {navLinks.map((link) => {
+                const isActive = pathname.startsWith(link.href)
+
+                return (
+                    <Link href={link.href} className={isActive? 'font-bold mr-4':"text-blue-700"} key={link.href} >
+                        {link.name}
+                    </Link >
+                )
+
+            })
+            }
+            {children}
+
+        </>
+    )
+}
