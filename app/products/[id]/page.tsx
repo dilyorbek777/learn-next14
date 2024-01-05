8777975aaee125b6702412cbef14235db9703465
dilyorbek777 <asfandiyorovdilyorbek@gmail.com>
2024-01-05 16:36:24 +0500

    metadata dynamic routes

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-
-export default function Detail({ params }:{
-    params:{
-        id:string
+import { Metadata } from "next"
+type Props = {
+    params: {
+        id: string
     }
-}) {
+}
+export const generateMetadata = async ({params}:Props): Promise<Metadata>=>{
+    const title =await new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve(`iphone ${params.id}`)
+        },100)
+    })
+    return {
+        title: `Product: ${title} `
+    }
+}
+
+
+
+export default function Detail({ params }: Props) {
     return (
         <div>Product Details page {params.id}</div>
     )
